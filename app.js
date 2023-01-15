2d5dbeeac444ad5dc6a5aa024c11cb3edc9c2745
AxmadAli <ilyosaxmadali@gmail.com>
2023-01-15 10:16:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+/*let ism = prompt("ismingizni kiriting");
+let yosh = +prompt("yoshingizni kiriting");
+// let birinchiHarf = ism.charAt(0).toUpperCase();
+// let davomi = ism.length.slice(1).toLowerCase();
+// let result = birinchiHarf + davomi;
+// alert(result);
+
+let tugulganYil = 2023 - yosh;
+let oy = yosh * 12;
+let hafta = ((yosh * 365) / 7).toFixed();
+let kun = yosh * 365;
+let soat = kun * 24;
+let daqiqa = soat * 60;
+let soniya = daqiqa * 60;
+
+alert(
+  `Salom ${ism} siz ${tugulganYil} yilda tugulgansiz va siz ${oy} oy, ${hafta} hafta, ${kun} kun, ${soat} soat, ${daqiqa} daqiqa, ${soniya} soniya yashagansiz !`
+);*/
+// for (let i=0; i<1;i++){
+//     console.log ('ilyos');
+// }
+
+// const favoriteColor = [];
+// favoriteColor[0] = prompt("What's your favorite color ? #1", "");
+// favoriteColor[1] = prompt("What's your favorite color ? #2", "");
+// favoriteColor[2] = prompt("What's your favorite color ? #3", "");
+// console.log(favoriteColor);
